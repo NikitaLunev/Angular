f89be08d18b9c9fda2c409c43c5ce89c9a18bf94
Nikita <lun9lol@gmail.com>
2022-12-01 20:39:50 +0200

    menu

---
+++ hello-world/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Привет мир';
+  description = 'Это мое первое Angular приложение!'
+}
