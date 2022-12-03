2811955fd45de0763ab57652a58c50fff5dc726e
Nikita <lun9lol@gmail.com>
2022-12-03 16:03:55 +0200

    finalFirst

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { COUNTRIES } from './countries.mock-data';
+import { Country } from './country.interfece';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Привет мир';
-  description = 'Это мое первое Angular приложение!'
+export class AppComponent implements OnInit {
+  countries: Country[] = COUNTRIES;
+  selectedCapital!: string
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.countries);
+  }
+
+  onSelect(contry: Country): void {
+    console.log(contry);
+    this.selectedCapital = contry.capital;
+  }
 }
